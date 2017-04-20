7fabbdf2368780ffc009c7004dabdfa03ea8cdc6
cverdi <cristina.verdi.tobon@gmail.com>
2017-04-20 17:59:25 +0200

    First commit

---
+++ src/main/java/com/spots/SpotsappApplication.java
@@ -0,0 +1,12 @@
+package com.spots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotsappApplication.class, args);
+	}
+}
